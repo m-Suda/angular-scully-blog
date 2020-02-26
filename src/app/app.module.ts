38e6c139379cbfdea3dc72fbfad5e2544737865a
masaks <oicwam.orz1228@gmail.com>
2020-02-26 23:27:56 +0900

    Angular Materialを導入

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { ScullyLibModule } from '@scullyio/ng-lib';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, ScullyLibModule],
+    imports: [BrowserModule, AppRoutingModule, ScullyLibModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
