49112a28b528a23668a7dbeeb0aa01c6fa2a3a71
masaks <oicwam.orz1228@gmail.com>
2020-03-02 02:17:05 +0900

    MaterialModuleにMatToolbarModuleを追加

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
-const materialModules = [MatButtonModule, MatIconModule];
+const materialModules = [MatToolbarModule, MatButtonModule, MatIconModule];
 
 @NgModule({
     declarations: [],
