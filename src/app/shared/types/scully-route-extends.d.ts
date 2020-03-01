719cab6c185a32f7114153b8c541ec4e8105f31e
masaks <oicwam.orz1228@gmail.com>
2020-03-02 02:14:53 +0900

    記事の属性に `thumbnail`, `date`を追加したので型定義の方にも追加。また `description` 項目が無かったのでこれも追加。

--- src/app/shared/types/scully-route-extends.d.ts
+++ src/app/shared/types/scully-route-extends.d.ts
@@ -2,4 +2,7 @@ import { ScullyRoute } from '@scullyio/ng-lib';
 
 export interface ScullyRouteExtendsType extends ScullyRoute {
     category?: string;
+    thumbnail?: string;
+    date?: string;
+    description?: string;
 }
