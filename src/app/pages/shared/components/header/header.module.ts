5f4309a5ca3d61fd504a279e51bd3d96a07e3779
masaks <oicwam.orz1228@gmail.com>
2020-06-14 22:43:37 +0900

    Featherアイコンあんまり使わなかったのでアンインストール。またわざわざMaterialモジュールから読み込まず必要なものだけ読み込むよう修正

--- src/app/pages/shared/components/header/header.module.ts
+++ src/app/pages/shared/components/header/header.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HeaderComponent } from './header.component';
-import { IconsModule } from '../../icons.module';
 
 const components = [HeaderComponent];
 
 @NgModule({
     declarations: [...components],
-    imports: [CommonModule, MatToolbarModule, IconsModule],
+    imports: [CommonModule, MatToolbarModule],
     exports: [...components],
 })
 export class HeaderModule {}
--- src/app/pages/shared/components/search-button/search-button.module.ts
+++ src/app/pages/shared/components/search-button/search-button.module.ts
@@ -1,20 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { SearchButtonComponent } from './search-button.component';
-import { MatButtonModule } from "@angular/material/button";
-import { IconsModule } from "../../icons.module";
-
-
 
 @NgModule({
-    declarations: [ SearchButtonComponent ],
-    exports: [
-        SearchButtonComponent
-    ],
-    imports: [
-        CommonModule,
-        MatButtonModule,
-        IconsModule
-    ]
+    declarations: [SearchButtonComponent],
+    exports: [SearchButtonComponent],
+    imports: [CommonModule, MatButtonModule, MatIconModule],
 })
-export class SearchButtonModule { }
+export class SearchButtonModule {}
